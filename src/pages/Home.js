49c860491b5079a1fec23ec362bc7b79d9ea1f92
Joy Antioho <j.wildfong@yahoo.com>
2026-09-01 07:29:29 -0400

    google-clone

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import './Home.css';
+import React from "react";
+import "./Home.css";
 import { Link } from "react-router-dom";
 import AppsIcon from "@mui/icons-material/Apps";
+import Avatar from "@mui/material/Avatar";
+import Search from '../pages/Search';
 
 
 function Home() {
@@ -9,25 +11,27 @@ function Home() {
     <div className="home">
       <div className="home__header">
         <div className="home__headerLeft">
-          <Link to='/about'>About</Link>
-          <Link to='/store'>Store</Link>
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
         </div>
         <div className="home__headerRight">
-          <Link to='/gmail'>Gmail</Link>
-          <Link to='/about'>Images</Link>
-        <AppsIcon />
-    
-       
-
-        </div>
-
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/about">Images</Link>
+          <AppsIcon />
+          <Avatar />
+       </div>
       </div>
 
       <div className="home__body">
-
+       <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+       alt=""
+       />
+       <div className="home__inputContainer">
+        <Search />
+       </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
---
+++ src/pages/Search.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Search from './Search.css';
+import MicIcon from '@mui/icons-material/Mic';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+function Search() {
+    return (
+     <div className='search'>
+        <div className='search__input'>
+            <SearchIcon className="search__inputIcon">
+             <input />
+            <MicIcon />
+            </SearchIcon>
+          
+        </div>
+     </div>
+    );
+}
+
+export default Search; 
