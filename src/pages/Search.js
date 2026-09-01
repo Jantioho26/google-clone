dcc41bf800ef524dcca1a7b99b35ba0d69bdb232
Joy Antioho <j.wildfong@yahoo.com>
2026-09-01 08:08:40 -0400

    google-clone

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Search.css';
 import MicIcon from '@mui/icons-material/Mic';
 import SearchIcon from '@mui/icons-material/Search';
 
 
+
 function Search() {
+    const [input, setInput] = useState('')
+
+    const search = e => {
+        e.preventDefault();
+    }
     return (
      <div className='search'>
         <div className='search__input'>
             <SearchIcon className="search__inputIcon" />
             <input />
             <MicIcon />
+        </div>
+        <div className="search__buttons">
+           <button onClick={search}>Google Search</button>
+           <button>I'm Feeling Lucky</button>
         </div>
      </div>
     );
