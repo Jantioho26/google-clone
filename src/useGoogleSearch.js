e6d11cd4f3d0edc0d82c8b959221309e5241db9a
Joy Antioho <j.wildfong@yahoo.com>
2026-09-02 15:43:33 -0400

    google-clone

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -3,27 +3,20 @@ import API_KEY from './Keys'
 
 const CONTEXT_KEY = "a48e5dbfcb0ce4f0e"
 
-const useGoogleSearch = () => {
+const useGoogleSearch = ( term ) => {
     const [data, setData] = useState(null);
 
     useEffect(() => {
       const fetchData = async() => {
-        fetch(
-            `https://www.googleapis.com/customsearch/v1?key=$
-            {API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+        fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
         )
-        .then(response => response.json())
-        .then(result => {
-            setData(result)
-        })
-      }
+        .then((res) => res.json())
+        .then((result) => setData(result));
+      };
       fetchData();
-    }, [term])
-  
+    }, [term]);
 
-   
- 
-  
+    return { data };
 }
 
 export default useGoogleSearch;
